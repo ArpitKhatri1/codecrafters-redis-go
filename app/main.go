75f89048b4c284a700e8aaab4de931987ac1e71f
ArpitKhatri1 <arpitkumarkhatri@gmail.com>
2025-10-27 11:39:57 +0530

    test pong conc

--- app/main.go
+++ app/main.go
@@ -9,6 +9,22 @@ import (
 var _ = net.Listen
 var _ = os.Exit
 
+func handleConnection(c net.Conn) {
+	defer c.Close()
+	buff := make([]byte, 1024)
+
+	for {
+		_, err := c.Read(buff)
+		if err != nil {
+			fmt.Println("Connection err", err)
+			return
+		}
+
+		c.Write([]byte("PONG"))
+	}
+
+}
+
 func main() {
 
 	l, err := net.Listen("tcp", "0.0.0.0:6379") //creates a Listener
@@ -16,6 +32,7 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+	defer l.Close()
 
 	//Creates a bidirectional channel
 
@@ -25,13 +42,6 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-
-	for {
-		buff := make([]byte, 1024)
-
-		c.Read(buff)
-
-		c.Write([]byte("+PONG\r\n"))
-	}
+	go handleConnection(c)
 
 }
