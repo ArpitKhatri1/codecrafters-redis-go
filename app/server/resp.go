618494c420785a51558d4e261b6c9c4bc078ea1a
ArpitKhatri1 <arpitkumarkhatri@gmail.com>
2025-10-29 23:57:09 +0530

    set and get

--- app/server/resp.go
+++ app/server/resp.go
@@ -1,13 +1,20 @@
 package server
 
 import (
+	"strconv"
 	"strings"
+	"sync"
 )
 
 const (
 	String = '+'
 	Bulk   = '$'
 	Array  = '*'
+)
+
+var (
+	store = make(map[string]string)
+	mu    sync.RWMutex
 )
 
 func ParseRESPInput(input string) string {
@@ -19,15 +26,34 @@ func ParseRESPInput(input string) string {
 		switch command {
 		case "echo":
 			for i := 3; i < len(commandArray)-1; i++ {
-
 				ans += commandArray[i]
 				ans += "\r\n"
 			}
 		case "ping":
 			ans += "+PONG\r\n"
+		case "set":
+			key := commandArray[4]
+			value := commandArray[6]
 
+			mu.Lock()
+			store[key] = value
+			mu.Unlock()
+
+			ans += "+OK\r\n"
+
+		case "get":
+			searchKey := commandArray[4]
+
+			mu.Lock()
+			value, ok := store[searchKey]
+			mu.Unlock()
+
+			if !ok {
+				ans += "$-1\r\n"
+			} else {
+				ans += "$" + strconv.Itoa(len(value)) + "\r\n" + value + "\r\n"
+			}
 		}
-
 	}
 	return ans
 
