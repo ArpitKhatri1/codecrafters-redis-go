8710365a42588ce5bcd0a03939c204d88066b7c8
ArpitKhatri1 <arpitkumarkhatri@gmail.com>
2025-10-29 23:13:30 +0530

    echo

--- app/server/resp.go
+++ app/server/resp.go
@@ -11,16 +11,26 @@ const (
 )
 
 func ParseRESPInput(input string) string {
-	commandArray := strings.Split(input, "\r\n")
 	ans := ""
-
-	if strings.ToLower(commandArray[2]) == "echo" {
-		for i := 3; i < len(commandArray)-1; i++ {
-
-			ans += commandArray[i]
-			ans += "\r\n"
+	switch input[0] {
+	case String:
+		formattedString := input[1 : len(input)-1]
+		if strings.ToLower(formattedString) == "ping" {
+			ans = "+PONG\r\n"
 		}
-	}
+	case Array:
+		commandArray := strings.Split(input, "\r\n")
+		ans := ""
 
+		if strings.ToLower(commandArray[2]) == "echo" {
+			for i := 3; i < len(commandArray)-1; i++ {
+
+				ans += commandArray[i]
+				ans += "\r\n"
+			}
+		}
+
+	}
 	return ans
+
 }
