d8841620abcf5d5563e052c6663507bf3d4a1ee6
ArpitKhatri1 <arpitkumarkhatri@gmail.com>
2025-10-31 23:35:48 +0530

    EXEC

--- app/RESP/resp.go
+++ app/RESP/resp.go
@@ -38,7 +38,7 @@ func (r *RESPParser) handlePING() string {
 }
 
 func (r *RESPParser) handleECHO() string {
-	temp := ""
+	temp := "$"
 	for i := 1; i < len(r.commandArray); i++ {
 		temp += r.commandArray[i]
 		temp += "\r\n"
